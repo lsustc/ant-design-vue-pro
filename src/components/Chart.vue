71daa386950b1de6c7543661c3d80e450c90028a
sl <844196322@qq.com>
2021-06-15 08:31:58 +0800

    feat

---
+++ src/components/Chart.vue
@@ -0,0 +1,49 @@
+<template>
+    <div ref="chartDom" style="height: 400px">
+       
+    </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+import { addListener, removeListener } from "resize-detector";
+import debounce from "lodash/debounce";
+export default {
+    props: {
+        option: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    created() {
+        this.resize = debounce(this.resize, 300);
+    },
+    watch: {
+        option(val) {
+            this.chart.setOption(val);
+        }
+    },
+    mounted() {
+        this.renderChart();
+        addListener(this.$refs.chartDom, this.resize)
+    },
+    methods: {
+        resize() {
+            console.log('resize')
+            this.chart.resize()
+        },
+        renderChart() {
+            this.chart = echarts.init(this.$refs.chartDom);
+            this.chart.setOption(this.option);
+        }
+    },
+    beforeDestroy() {
+        removeListener(this.$refs.chartDom, this.resize);
+        this.chart.dispose();
+        this.chart = null;
+    }
+}
+</script>
+<style>
+
+</style>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>
-        分析页
-    </div>
+  <div>
+      <Chart :option="option" style="height: 400px"/>
+  </div>
 </template>
 
 <script>
+import Chart from "../../components/Chart.vue";
 export default {
-    
-}
+  components: {
+    Chart,
+  },
+  data() {
+      // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+      return {
+          option
+      }
+  }
+};
 </script>
 <style>
-
 </style>
