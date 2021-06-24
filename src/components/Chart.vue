980f6a2695f7dc7a8c3cf06cab6ca5d93c91d9af
sl <844196322@qq.com>
2021-06-24 09:04:32 +0800

    优化打包构建

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
-import * as echarts from "echarts";
+import echarts from "echarts/lib/echarts";
+import 'echarts/lib/chart/bar';
+import 'echarts/lib/component/title';
 import { addListener, removeListener } from "resize-detector";
 import debounce from "lodash/debounce";
 export default {
